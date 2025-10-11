0715f7dfd24b3c2dd2daf631b27829d9fc95ee4f
deep3576 <deep3576@gmail.com>
2025-10-11 12:10:44 -0400

    Website update , No Log in yet

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,6 @@ import os
 # Ensure project path is on sys.path (adjust if your project folder differs)
 project_home = os.path.abspath(os.path.dirname(__file__))
 if project_home not in sys.path:
-    sys.path.insert(0, project_home)
+    sys.path.insert(0, '/home/deep3576/KingsmanConstruction')
 
 from app import app as application  # PythonAnywhere expects `application`
