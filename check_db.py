b35c5442ef415c0516eea8d34fafe92ffdd29481
deep3576 <deep3576@gmail.com>
2025-10-15 13:15:14 -0400

    Enhancements Admin panel

--- check_db.py
+++ check_db.py
@@ -1,5 +1,7 @@
 import pymysql
-conn = pymysql.connect(host="127.0.0.1", port=3306,
+from config import Config
+
+conn = pymysql.connect(host=deep3576.mysql.pythonanywhere-services.com, port=3306,
                        user="deep3576", password="Gmsshn!43",
                        database="deep3576$ProductionDB", charset="utf8mb4")
 with conn.cursor() as cur:
